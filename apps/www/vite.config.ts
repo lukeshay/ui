a535156d7e4212bbe87710563bf339cd9099fb57
Luke Shay <shay.luke17@gmail.com>
2023-08-02 10:19:17 -0500

    skip format

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -4,6 +4,5 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/ui/",
 	plugins: [million.vite({ auto: true, mute: true, optimize: true }), react()],
 })
