56bbc8952d3bfa51b5b9c83203f9616facde1566
Luke Shay <shay.luke17@gmail.com>
2023-08-02 13:56:20 -0500

    dont use million for now

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -1,9 +1,11 @@
-import million from "million/compiler"
+// import million from "million/compiler"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
+
+// million.vite({ auto: true, mute: true, optimize: true }), 
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/ui/",
-	plugins: [million.vite({ auto: true, mute: true, optimize: true }), react()],
+	plugins: [react()],
 })
