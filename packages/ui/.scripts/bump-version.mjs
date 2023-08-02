6ecd6fa654fb0bc3ab2f560ca2ebba3483d17707
Luke Shay <shay.luke17@gmail.com>
2023-08-02 12:43:50 -0500

    give write perms

--- packages/ui/.scripts/bump-version.mjs
+++ packages/ui/.scripts/bump-version.mjs
@@ -1,24 +1,23 @@
 #!/usr/bin/env node
 
+import { execSync } from "node:child_process"
 import { writeFile } from "node:fs/promises"
 import { argv } from "node:process"
 import { inc, parse } from "semver"
 
 import packageJson from "../package.json" assert { type: "json" }
 
-const run = async () => {
-	const oldVersion = packageJson.version
-	const version = parse(packageJson.version)
-	// eslint-disable-next-line @typescript-eslint/no-magic-numbers
-	const newVersion = inc(version, argv[2] ?? "patch")
+const oldVersion = packageJson.version
+const version = parse(packageJson.version)
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+const newVersion = inc(version, argv[2] ?? "patch")
 
-	packageJson.version = newVersion
+packageJson.version = newVersion
 
-	// eslint-disable-next-line no-console
-	console.log(`Bumping version from ${oldVersion} to ${newVersion}`)
+// eslint-disable-next-line no-console
+console.log(`Bumping version from ${oldVersion} to ${newVersion}`)
 
-	// eslint-disable-next-line @typescript-eslint/no-magic-numbers
-	await writeFile("./package.json", JSON.stringify(packageJson, undefined, 2))
-}
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+await writeFile("./package.json", JSON.stringify(packageJson, undefined, 2))
 
-void run()
+execSync(`git tag v${newVersion}`)
--- packages/ui/.scripts/clear-package-json.mjs
+++ packages/ui/.scripts/clear-package-json.mjs
@@ -5,26 +5,21 @@ import { env } from "node:process"
 
 import packageJson from "../package.json" assert { type: "json" }
 
-// eslint-disable-next-line max-statements
-const run = async () => {
-	if (!env.SKIP_UPDATE) {
-		packageJson.dependencies = undefined
-		packageJson.typesVersions = undefined
-		packageJson.exports = undefined
-	}
-
-	packageJson.peerDependencies = undefined
-	packageJson.peerDependenciesMeta = undefined
-	packageJson.devDependencies.tailwindcss = undefined
-	packageJson.devDependencies["@radix-ui/react-icons"] = undefined
-	packageJson.devDependencies["lucide-react"] = undefined
-	packageJson.devDependencies.react = undefined
-	packageJson.devDependencies["react-dom"] = undefined
-	packageJson.devDependencies["@types/react"] = undefined
-	packageJson.devDependencies["@types/react-dom"] = undefined
-
-	// eslint-disable-next-line @typescript-eslint/no-magic-numbers
-	await writeFile("./package.json", JSON.stringify(packageJson, undefined, 2))
+if (!env.SKIP_UPDATE) {
+	packageJson.dependencies = undefined
+	packageJson.typesVersions = undefined
+	packageJson.exports = undefined
 }
 
-void run()
+packageJson.peerDependencies = undefined
+packageJson.peerDependenciesMeta = undefined
+packageJson.devDependencies.tailwindcss = undefined
+packageJson.devDependencies["@radix-ui/react-icons"] = undefined
+packageJson.devDependencies["lucide-react"] = undefined
+packageJson.devDependencies.react = undefined
+packageJson.devDependencies["react-dom"] = undefined
+packageJson.devDependencies["@types/react"] = undefined
+packageJson.devDependencies["@types/react-dom"] = undefined
+
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+await writeFile("./package.json", JSON.stringify(packageJson, undefined, 2))
