a2f21e5d3a3bd6a03b353d2dc4b35cd0f281f717
Luke Shay <shay.luke17@gmail.com>
2023-08-02 10:20:19 -0500

    add path again

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/ui/",
 	plugins: [million.vite({ auto: true, mute: true, optimize: true }), react()],
 })
